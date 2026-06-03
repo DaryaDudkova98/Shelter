45805cf672a5be96392f678ae1fb2d260a1e90cc
DaryaDudkova98 <dudkovadaryadmitrievna@mail.ru>
2026-06-03 21:57:57 +0300

    A shelter folder containing project files has been created.

---
+++ shelter/index.js
@@ -0,0 +1,8 @@
+console.log(
+    'self-assessment criteria:\n' +
+    '1. A new branch, shelter, has been created from main\n' +
+    '2. Created a shelter folder inside the branch\n' +
+    '3. Created index.html and index.js files in the shelter folder\n' +
+    '4. Output to the browser console has been configured\n' +
+    '5. Работа развернута через gh-pages\n'
+)
